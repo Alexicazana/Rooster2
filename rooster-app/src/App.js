c0a7366673141a17379bd233a46e1d9c62d1044d
Alexicazana <55753055+Alexicazana@users.noreply.github.com>
2022-01-29 19:53:04 -0500

    alex changes

--- rooster-app/src/App.js
+++ rooster-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './roosterlogo.jpeg';
 import './App.css';
+import Navication from './components/NavBar';
 import NavBar from './components/NavBar';
 
 class App extends Component {
@@ -8,7 +9,7 @@ class App extends Component {
     return (
       <div>
         <NavBar>
-          
+
         </NavBar>
       </div>
     );
--- rooster-app/src/components/NavBar.js
+++ rooster-app/src/components/NavBar.js
@@ -2,6 +2,46 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+//import { NavLink } from "react-router-dom";
+
+
+/*
+const NavBar = () => {
+    return (
+        <div className="navigation">
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <div className="container">
+                    <NavLink className="navbar-brand" to="/">
+                        React Homepage 
+                    </NavLink>
+                </div>
+                <div>
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/login">
+                                Login
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/alarms">
+                                Alarms
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+
+} 
+
+*/
+
 
 const NavBar = () => {
     return(
@@ -16,5 +56,23 @@ const NavBar = () => {
         </div>
 
     )
-}
+} 
 export default NavBar;
+
+
+
+
+
+<Stack.Screen
+    name="HomeScreen"
+    component={HomeScreen}
+    options={{
+    headerRight: () => (
+        <Button
+        onPress={() => alert('Clicked !!')}
+        title="menu"
+        color="#000"
+        />
+    ),
+    }}
+/>
