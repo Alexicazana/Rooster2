9a10b851ee1e198e391b007ada03e6af82e140be
Eve Gagnon <emdgagnon@gmail.com>
2022-01-30 03:19:50 -0500

    added navbar and sidebar

--- rooster-app/src/components/Navbar/Navbar.js
+++ rooster-app/src/components/Navbar/Navbar.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import {FaBars} from 'react-icons/fa';
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+} from './NavbarElements';
 
-const NavBar = () => {
-  return <div></div>;
+const NavBar = ({toggle}) => {
+  return <>
+    <Nav>
+        <NavbarContainer>
+            <NavLogo to='/'>Rooster</NavLogo>
+            <MobileIcon onClick={toggle}>
+                <FaBars/>
+            </MobileIcon>
+            <NavMenu>
+                <NavItem>
+                    <NavLinks to='alarms'>Alarms</NavLinks>
+                </NavItem>
+            </NavMenu>
+            <NavBtn>
+                <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+            </NavBtn>
+        </NavbarContainer>
+    </Nav>
+  </>;
 };
 
 export default NavBar;
